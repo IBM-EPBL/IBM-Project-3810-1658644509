542203d8318342b07f3d67c462dc341fcf6ce188
Suba-V-39 <113596748+Suba-V-39@users.noreply.github.com>
2022-11-19 19:43:41 +0530

    Add files via upload

---
+++ Final Deliverables/final code/app.py
@@ -0,0 +1,162 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+import ibm_db
+import bcrypt
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=b0aebb68-94fa-46ec-a1fc-1c999edb6187.c3n41cmd0nqnrk39u98g.databases.appdomain.cloud;PORT=31249;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=lyj19879;PWD=O5FeEzqOfN27OpWt",'','')
+# url_for('static', filename='style.css')
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route("/",methods=['GET']) 
+def home():
+    if 'email' not in session:
+      return redirect(url_for('register'))
+    return render_template('index.html',name='Home')
+
+@app.route("/registerUser",methods=['GET','POST'])
+def register():
+  if request.method == 'POST':
+    username = request.form['username']
+    email = request.form['email']
+    phoneno = request.form['phoneno']
+    password = request.form['password']
+
+    if not username or not email or not phoneno or not password:
+      return render_template('registerUser.html',error='Please fill all fields')
+    hash=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+    query = "SELECT * FROM user WHERE email=? OR phoneno=?"
+    stmt = ibm_db.prepare(conn, query)
+    ibm_db.bind_param(stmt,1,email)
+    ibm_db.bind_param(stmt,2,phoneno)
+    ibm_db.execute(stmt)
+    isUser = ibm_db.fetch_assoc(stmt)
+    if not isUser:
+      insert_sql = "INSERT INTO user(username, email, phoneno, password) VALUES (?,?,?,?)"
+      prep_stmt = ibm_db.prepare(conn, insert_sql)
+      ibm_db.bind_param(prep_stmt, 1, username)
+      ibm_db.bind_param(prep_stmt, 2, email)
+      ibm_db.bind_param(prep_stmt, 3, phoneno)
+      ibm_db.bind_param(prep_stmt, 4, hash)
+      ibm_db.execute(prep_stmt)
+      return redirect(url_for('home'))
+    else:
+      return render_template('registerUser.html',error='Invalid Credentials')
+
+  return render_template('registerUser.html',name='Home')
+
+@app.route("/loginUser",methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+      email = request.form['email']
+      password = request.form['password']
+      print("entered into post")
+
+      if not email or not password:
+        return render_template('loginUser.html',error='Please fill all fields')
+      query = "SELECT * FROM user WHERE email=?"
+      stmt = ibm_db.prepare(conn, query)
+      ibm_db.bind_param(stmt,1,email)
+      ibm_db.execute(stmt)
+      isUser = ibm_db.fetch_assoc(stmt)
+      print(isUser,password)
+
+      if not isUser:
+        return render_template('loginUser.html',error='Invalid Credentials')
+      
+      isPasswordMatch = bcrypt.checkpw(password.encode('utf-8'),isUser['PASSWORD'].encode('utf-8'))
+
+      if not isPasswordMatch:
+        return render_template('loginUser.html',error='Invalid Credentials')
+
+      session['email'] = isUser['EMAIL']
+      return redirect(url_for('home'))
+
+    return render_template('loginUser.html',name='Home')
+
+    
+
+
+@app.route("/registerAdmin",methods=['GET','POST'])
+def registerAd():
+  if request.method == 'POST':
+    adminname = request.form['adminname']
+    email = request.form['email']
+    phoneno = request.form['phoneno']
+    password = request.form['password']
+
+    if not adminname or not email or not phoneno or not password:
+      return render_template('registerAdmin.html',error='Please fill all fields')
+    hash=bcrypt.hashpw(password.encode('utf-8'),bcrypt.gensalt())
+    query = "SELECT * FROM admin WHERE email=? OR phoneno=?"
+    stmt = ibm_db.prepare(conn, query)
+    ibm_db.bind_param(stmt,1,email)
+    ibm_db.bind_param(stmt,2,phoneno)
+    ibm_db.execute(stmt)
+    isUser = ibm_db.fetch_assoc(stmt)
+    if not isUser:
+      insert_sql = "INSERT INTO admin(adminname, email, phoneno, password) VALUES (?,?,?,?)"
+      prep_stmt = ibm_db.prepare(conn, insert_sql)
+      ibm_db.bind_param(prep_stmt, 1, adminname)
+      ibm_db.bind_param(prep_stmt, 2, email)
+      ibm_db.bind_param(prep_stmt, 3, phoneno)
+      ibm_db.bind_param(prep_stmt, 4, hash)
+      ibm_db.execute(prep_stmt)
+      return redirect(url_for('stock'))
+    else:
+      return render_template('registerAdmin.html',error='Invalid Credentials')
+
+  return render_template('registerAdmin.html',name='Home')
+
+@app.route("/loginAdmin",methods=['GET','POST'])
+def loginAd():
+    if request.method == 'POST':
+      email = request.form['email']
+      password = request.form['password']
+
+      if not email or not password:
+        return render_template('loginAdmin.html',error='Please fill all fields')
+      query = "SELECT * FROM admin WHERE email=?"
+      stmt = ibm_db.prepare(conn, query)
+      ibm_db.bind_param(stmt,1,email)
+      ibm_db.execute(stmt)
+      isUser = ibm_db.fetch_assoc(stmt)
+      print(isUser,password)
+
+      if not isUser:
+        return render_template('loginAdmin.html',error='Invalid Credentials')
+      
+      isPasswordMatch = bcrypt.checkpw(password.encode('utf-8'),isUser['PASSWORD'].encode('utf-8'))
+
+      if not isPasswordMatch:
+        return render_template('loginAdmin.html',error='Invalid Credentials')
+
+      session['email'] = isUser['EMAIL']
+      return redirect(url_for('stock'))
+
+    return render_template('loginAdmin.html',name='Home')
+
+
+@app.route('/homepage')
+def homepage():
+  return render_template('home.html')
+
+@app.route('/shop')
+def shop():
+  return render_template('Shop.html')
+
+@app.route('/About', methods=['GET','POST'])
+def About():
+  return render_template('About.html')
+
+@app.route('/stock', methods=['GET','POST'])
+def stock():
+  return render_template('Stock.html')  
+
+@app.route('/logout')
+def logout():
+    session.pop('email', None)
+    return redirect(url_for('login'))
+    
+if __name__ == "__main__":
+    app.run(debug=True)
